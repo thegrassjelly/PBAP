ab537f92bbd3688dd83d9237ceb7f5eaca1ee2d1
Robert Steven Tomas <steven.tomas@live.com>
2016-12-25 22:10:51 +0800

    Init

---
+++ App_Code/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Helper
+/// </summary>
+public class Helper
+{
+    public Helper()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
